24bc1b50e4500b91dcbd92af4c45813291bd4671
Kyle <kyle.kopatz@gmail.com>
2020-12-01 19:40:53 -0600

    all tests except toString

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -6,25 +6,42 @@ namespace TechJobsTests
     [TestClass]
     public class JobTests
     {
+        Job job1;
+        Job job2;
+        Job job3;
+        Job job4;
+        [TestInitialize]
+        public void CreateJobObjects()
+        {
+            job1 = new Job();
+            job2 = new Job();
+            job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        }
+
         [TestMethod]
         public void TestSettingJobId()
-        {
-            Job job1 = new Job();
-            Job job2 = new Job();
-
+        { 
             Assert.IsFalse(job1.Id == job2.Id);
         }
 
         [TestMethod]
         public void TestJobConstructorSetsAllFields()
         {
-            Job jobTest = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-            Assert.AreEqual("Product Tester", jobTest.Name);
-            Assert.AreEqual("ACME", jobTest.EmployerName);
-            Assert.AreEqual("Desert", jobTest.EmployerLocation);
-            Assert.AreEqual("Quality", jobTest.JobType);
-            Assert.AreEqual("Persistence", jobTest.JobCoreCompetency);
-
+            
+            Assert.AreEqual("Product tester", job3.Name);
+            Assert.AreEqual("ACME", job3.EmployerName.Value);
+            Assert.AreEqual("Desert", job3.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", job3.JobType.Value);
+            Assert.AreEqual("Persistence", job3.JobCoreCompetency.value);
         }
+    
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Assert.IsFalse(job3.Equals(job4));
+        }
+
+
     }
 }
